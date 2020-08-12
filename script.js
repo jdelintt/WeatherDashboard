7cefb30168dbda9bd72af81bcaae30ee10c746cb
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-11 18:31:30 -0700

    created ajax call for weather api

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
   $(".five-day-forecast").append("Hello World");
-
-  $.ajax({
-    url: latLongUrl,
-    method: "GET",
-  }).then((response) => {});
+  $(".start-search").on("click", function () {
+    console.log($("#citysearch").val());
+    var cityParam = $("#citysearch").val();
+    var cityParamUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityParam +
+      "&appid=927924e4c73455c7286d71a6b1b45a4c";
+    $.ajax({
+      url: cityParamUrl,
+      method: "GET",
+    }).then((response) => {
+      console.log(response);
+    });
+  });
 });
