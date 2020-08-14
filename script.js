2b521fa15feee93bc81b3e285f71b701548b23e1
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-13 20:28:44 -0700

    set each city query to local storage
    Co-authored-by: Wesley Clements <wesley.clements@gmail.com>

--- script.js
+++ script.js
@@ -1,78 +1,118 @@
 $(document).ready(function () {
-  $(".five-day-forecast").append("Hello World");
-  $(".start-search").on("click", function () {
-    console.log($("#citysearch").val());
-    var cityParam = $("#citysearch").val();
-    var weatherBtn = $("<button>");
-    weatherBtn.append(cityParam);
-    weatherBtn.attr("text", cityParam);
-    $(".citylist").append(weatherBtn);
+  let history = [];
+  let historyJSON = localStorage.getItem("cities");
+  if (historyJSON != null) {
+    history = JSON.parse(historyJSON);
+  }
+
+  renderSearchHistory();
+
+  function renderSearchHistory() {
+    $("#city-list").empty();
+    history.forEach(function (city) {
+      var li = $("<li>");
+      li.addClass("list-group-item");
+      li.text(city);
+      li.click(function (event) {
+        renderSearchResult(city);
+      });
+      $("#city-list").append(li);
+    });
+  }
+
+  function renderSearchResult(city) {
+    $(".appendweather").empty();
+    $(".five-day-forecast").empty();
+
     var cityParamUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityParam +
+      city +
       "&appid=927924e4c73455c7286d71a6b1b45a4c&count=5";
 
-    //   First ajax call for weather API
+    //   First ajax call for weather API fahrenheit, humdity, and wind speed
     $.ajax({
       url: cityParamUrl,
       method: "GET",
-    }).then((response) => {
-      console.log(response);
-      var ptagtemp = $("<p>");
-      var ptaghumidity = $("<p>");
-      var ptagwind = $("<p>");
-      var temperature = response.main.temp;
-      temperature = temperature - 273;
-      temperature = temperature * (9 / 5);
-      temperature = Math.floor(temperature + 32);
-      var humidity = response.main.humidity;
-      var windSpeed = response.wind.speed;
-      ptagtemp.append("Temperature is " + temperature + " F");
-      ptaghumidity.append("Humidity: " + humidity + "%");
-      ptagwind.append(windSpeed + " MPH");
-      $(".appendweather").append(ptagtemp, ptaghumidity, ptagwind);
-      var lat = response.coord.lat;
-      var long = response.coord.lon;
+    })
+      .then((response) => {
+        history.unshift(response.name);
+        history = [...new Set(history)];
+        localStorage.setItem("cities", JSON.stringify(history));
+        renderSearchHistory();
 
-      var uvParam =
-        "http://api.openweathermap.org/data/2.5/uvi?appid=927924e4c73455c7286d71a6b1b45a4c&lat=" +
-        lat +
-        "&lon=" +
-        long;
-      $.ajax({
-        url: uvParam,
-        method: "GET",
-      }).then((response) => {
         console.log(response);
-        var ptagUVI = $("<p>");
-        ptagUVI.append("UVI:" + response.value);
-        $(".appendweather").append(ptagUVI);
+        var ptagtemp = $("<p>");
+        var ptaghumidity = $("<p>");
+        var ptagwind = $("<p>");
+        var temperature = response.main.temp;
+        temperature = temperature - 273;
+        temperature = temperature * (9 / 5);
+        temperature = Math.floor(temperature + 32);
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        ptagtemp.append("Temperature is " + temperature + " F");
+        ptaghumidity.append("Humidity: " + humidity + "%");
+        ptagwind.append(windSpeed + " MPH");
+        $(".appendweather").append(ptagtemp, ptaghumidity, ptagwind);
+        var lat = response.coord.lat;
+        var long = response.coord.lon;
 
-        var urlFiveDay =
-          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        var uvParam =
+          "http://api.openweathermap.org/data/2.5/uvi?appid=927924e4c73455c7286d71a6b1b45a4c&lat=" +
           lat +
           "&lon=" +
-          long +
-          "&appid=927924e4c73455c7286d71a6b1b45a4c";
+          long;
+
+        // Ajax call for UV value
         $.ajax({
-          url: urlFiveDay,
+          url: uvParam,
           method: "GET",
         }).then((response) => {
-          for (let i = 0; i < response.daily.length; i++) {
-            var ptag = $("<p>");
-            // var date = "Today's date is:" + response.daily[i].temp.day;
-            var temp = response.daily[i].temp.day;
-            temp = temp - 273;
-            temp = temp * (9 / 5);
-            temp = "Temp: " + Math.floor(temp + 32);
-            var humidity = "Humidity: " + response.daily[i].humidity;
-
-            ptag.append(temp, humidity);
-            $(".five-day-forecast").append(ptag);
-          }
           console.log(response);
+          var ptagUVI = $("<p>");
+          ptagUVI.append("UVI:" + response.value);
+          $(".appendweather").append(ptagUVI);
+
+          var urlFiveDay =
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat +
+            "&lon=" +
+            long +
+            "&appid=927924e4c73455c7286d71a6b1b45a4c";
+
+          // Ajax call for five day forecast
+
+          $.ajax({
+            url: urlFiveDay,
+            method: "GET",
+          }).then((response) => {
+            for (let i = 0; i < response.daily.length; i++) {
+              var ptag = $("<p>");
+              // var date = "Today's date is:" + response.daily[i].temp.day;
+              var temp = response.daily[i].temp.day;
+              temp = temp - 273;
+              temp = temp * (9 / 5);
+              temp = "Temp: " + Math.floor(temp + 32);
+              var humidity = "Humidity: " + response.daily[i].humidity;
+
+              ptag.append(temp, humidity);
+              $(".five-day-forecast").append(ptag);
+            }
+            console.log(response);
+          });
         });
+      })
+      .catch(function (error) {
+        console.error(error);
       });
-    });
+  }
+
+  $(".five-day-forecast").append("Hello World");
+  $("#search-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $("#citysearch").val();
+
+    renderSearchResult(city);
   });
 });
